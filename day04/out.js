17cb35484670c4cee2049ee92682b7ce10fcf604
YangHyeonJun <124110658+yang960920@users.noreply.github.com>
2023-02-02 17:34:23 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,18 @@
+function 빨래하다() {
+    console.log('1. 먼지를 털자.')
+    console.log('2. 물에 집어 넣는다')
+    console.log('3. 비누를 바른다.')
+    console.log('4. 오물조물 비빈다.')
+    console.log('5. 물에 여러번 행군다.')
+    console.log('6. 물을 짠다')
+    console.log('7. 옷을 넌다')
+}
+
+function 청소하다() {
+    console.log('1.창문을 연다.')
+    console.log('2.청소기의 버튼을 누른다.')
+    console.log('3.집안 구석구석 민다')
+    console.log('4.물청소기를 집는다.')
+    console.log('5.구석구석 민다')
+    console.log('6.털썩 주저앉는다.')      
+}
---
+++ day04/out2.js
@@ -0,0 +1,14 @@
+function 여행하다() {
+    console.log('1.생필품을 정리해서 싼다')
+    console.log('2.사진이 잘나오는 옷을 넣는다')
+    console.log('3.렌트카를 빌린다')
+    console.log('4.숙소를 예약한다')
+    console.log('5.탈진 할정도로 논다')
+}
+function 쉬다() {
+    console.log('1.이불을 소중하게 들어올린다')
+    console.log('2.전자기기 거치대를 셋팅한다')
+    console.log('3.테블릿은 넷플릭스를 킨다')
+    console.log('4.핸드폰은 모바일 게임을 켠다')
+    console.log('5.하루를 삭제한다')
+}
